cfb5872ba953488f453f2a304eaed43787c197b4
jfoll2001 <106092622+jfoll2001@users.noreply.github.com>
2022-06-14 14:35:35 -0400

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var app = express();
 
-var requireAbout = require('./routes/about');
+var requireAbout = 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/index'));
-app.use('/about', requireAbout);
+app.use('/about', require('./routes/about'));
 app.use('/addBook', require('./routes/addBook'));
 
 // catch 404 and forward to error handler
